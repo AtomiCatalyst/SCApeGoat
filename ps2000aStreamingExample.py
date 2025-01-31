52377a7a4aefcbc94931f5083613abb00f8f10e2
Nat <dynkon@gmail.com>
2025-01-31 11:15:44 -0500

    Streaming mode tests

--- ps2000aStreamingExample.py
+++ ps2000aStreamingExample.py
@@ -31,9 +31,8 @@ analogue_offset = 0.0
 # channel = PS2000A_CHANNEL_A = 0
 # enabled = 1
 # coupling type = PS2000A_DC = 1
-# range = PS2000A_2V = 7
 # analogue offset = 0 V
-channel_range = ps.PS2000A_RANGE['PS2000A_2V']
+channel_range = ps.PS2000A_RANGE['PS2000A_5V']
 status["setChA"] = ps.ps2000aSetChannel(chandle,
                                         ps.PS2000A_CHANNEL['PS2000A_CHANNEL_A'],
                                         enabled,
@@ -47,7 +46,6 @@ assert_pico_ok(status["setChA"])
 # channel = PS2000A_CHANNEL_B = 1
 # enabled = 1
 # coupling type = PS2000A_DC = 1
-# range = PS2000A_2V = 7
 # analogue offset = 0 V
 status["setChB"] = ps.ps2000aSetChannel(chandle,
                                         ps.PS2000A_CHANNEL['PS2000A_CHANNEL_B'],
@@ -57,6 +55,15 @@ status["setChB"] = ps.ps2000aSetChannel(chandle,
                                         analogue_offset)
 assert_pico_ok(status["setChB"])
 
+
+status["setTriggerB"] = ps.ps2000aSetSimpleTrigger(chandle,
+                                                  enabled,
+                                                  ps.PS2000A_CHANNEL['PS2000A_CHANNEL_B'],
+                                                  channel_range, #5V
+                                                  2, #rising edge
+                                                  0, #delay
+                                                  0) #auto trigger
+assert_pico_ok(status["setTriggerB"])
 # Size of capture
 sizeOfOneBuffer = 500
 numBuffersToCapture = 10
@@ -65,7 +72,6 @@ totalSamples = sizeOfOneBuffer * numBuffersToCapture
 
 # Create buffers ready for assigning pointers for data collection
 bufferAMax = np.zeros(shape=sizeOfOneBuffer, dtype=np.int16)
-bufferBMax = np.zeros(shape=sizeOfOneBuffer, dtype=np.int16)
 
 memory_segment = 0
 
@@ -86,22 +92,6 @@ status["setDataBuffersA"] = ps.ps2000aSetDataBuffers(chandle,
                                                      ps.PS2000A_RATIO_MODE['PS2000A_RATIO_MODE_NONE'])
 assert_pico_ok(status["setDataBuffersA"])
 
-# Set data buffer location for data collection from channel B
-# handle = chandle
-# source = PS2000A_CHANNEL_B = 1
-# pointer to buffer max = ctypes.byref(bufferBMax)
-# pointer to buffer min = ctypes.byref(bufferBMin)
-# buffer length = maxSamples
-# segment index = 0
-# ratio mode = PS2000A_RATIO_MODE_NONE = 0
-status["setDataBuffersB"] = ps.ps2000aSetDataBuffers(chandle,
-                                                     ps.PS2000A_CHANNEL['PS2000A_CHANNEL_B'],
-                                                     bufferBMax.ctypes.data_as(ctypes.POINTER(ctypes.c_int16)),
-                                                     None,
-                                                     sizeOfOneBuffer,
-                                                     memory_segment,
-                                                     ps.PS2000A_RATIO_MODE['PS2000A_RATIO_MODE_NONE'])
-assert_pico_ok(status["setDataBuffersB"])
 
 # Begin streaming mode:
 sampleInterval = ctypes.c_int32(250)
@@ -129,7 +119,6 @@ print("Capturing at sample interval %s ns" % actualSampleIntervalNs)
 
 # We need a big buffer, not registered with the driver, to keep our complete capture in.
 bufferCompleteA = np.zeros(shape=totalSamples, dtype=np.int16)
-bufferCompleteB = np.zeros(shape=totalSamples, dtype=np.int16)
 nextSample = 0
 autoStopOuter = False
 wasCalledBack = False
@@ -137,21 +126,22 @@ wasCalledBack = False
 
 def streaming_callback(handle, noOfSamples, startIndex, overflow, triggerAt, triggered, autoStop, param):
     global nextSample, autoStopOuter, wasCalledBack
-    wasCalledBack = True
-    destEnd = nextSample + noOfSamples
-    sourceEnd = startIndex + noOfSamples
-    bufferCompleteA[nextSample:destEnd] = bufferAMax[startIndex:sourceEnd]
-    bufferCompleteB[nextSample:destEnd] = bufferBMax[startIndex:sourceEnd]
-    nextSample += noOfSamples
-    if autoStop:
-        autoStopOuter = True
+    if triggered:
+        print(triggerAt)
+        wasCalledBack = True
+        destEnd = nextSample + noOfSamples
+        sourceEnd = startIndex + noOfSamples
+        bufferCompleteA[nextSample:destEnd] = bufferAMax[startIndex:sourceEnd]
+        nextSample += noOfSamples
 
 
 # Convert the python function into a C function pointer.
 cFuncPtr = ps.StreamingReadyType(streaming_callback)
 
+triggered = False
+
 # Fetch data from the driver in a loop, copying it out of the registered buffers and into our complete one.
-while nextSample < totalSamples and not autoStopOuter:
+while nextSample < totalSamples and not triggered:
     wasCalledBack = False
     status["getStreamingLastestValues"] = ps.ps2000aGetStreamingLatestValues(chandle, cFuncPtr, None)
     if not wasCalledBack:
@@ -170,14 +160,12 @@ assert_pico_ok(status["maximumValue"])
 
 # Convert ADC counts data to mV
 adc2mVChAMax = adc2mV(bufferCompleteA, channel_range, maxADC)
-adc2mVChBMax = adc2mV(bufferCompleteB, channel_range, maxADC)
-
+transformed_vals = np.array(adc2mVChAMax)
 # Create time data
 time = np.linspace(0, (totalSamples-1) * actualSampleIntervalNs, totalSamples)
 
 # Plot data from channel A and B
 plt.plot(time, adc2mVChAMax[:])
-plt.plot(time, adc2mVChBMax[:])
 plt.xlabel('Time (ns)')
 plt.ylabel('Voltage (mV)')
 plt.show()
